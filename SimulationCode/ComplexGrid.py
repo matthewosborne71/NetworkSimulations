44581df74a40f4624679157cf05b0eb60be11cc7
Matthew Osborne <matthewosborne71@gmail.com>
2019-03-05 14:39:39 -0500

    Added code for square grid simulations

---
+++ SimulationCode/ComplexGrid.py
@@ -0,0 +1,63 @@
+import CreateNetworks as CN
+import Simulation as S
+import logging
+import numpy as np
+import Path
+
+Ns = [40,50,100,250,500]
+Ms = [25,20,10,4,2]
+Periodic = [True,False]
+
+Thresholds = np.arange(0.1,0.55,0.05)
+betas = [1.5]
+gamma = 1
+
+NumSims = 50
+InitialFrac = 0.01
+StoppingTime = 5
+
+path = Path.GetPath()
+
+logging.basicConfig(filename = path + "Logs/ComplexSimSquareGrid.log",
+                    format = '%(asctime)s - %(message)s',
+                    level = logging.INFO)
+
+f = open(path + "SimulationResults/ComplexContagionSimulations_SquareGrid.csv","w+")
+
+f.write("N,M,Periodic,Threshold,beta,SimNum,EventTime,Event,CurrentI\n")
+
+TotalSims = len(Ns) * len(Periodic) * len(betas) * len(Thresholds) * NumSims
+CurrentSim = float(1)
+
+logging.info("About to run simple sims for Square Grid.")
+
+for P in Periodic:
+    for k in range(len(Ns)):
+        for beta in betas:
+            for Thresh in Thresholds:
+                for i in range(1,NumSims+1):
+                    logging.info("Currently at " + str(100*CurrentSim/float(TotalSims)) + "% of the Simulations Done.\n")
+                    CurrentSim = CurrentSim + 1
+
+                    G = CN.GridGraph(Ns[k],Ms[k],P)
+
+                    Times,Events,Is = S.ComplexSim(G,InitialFrac,StoppingTime,gamma,beta,Thresh,"Frac")
+
+                    for j in range(len(Times)):
+                        n = str(Ns[k])
+                        m = str(Ms[k])
+                        p = str(P)
+                        Beta = str(beta)
+                        SimNum = str(i)
+                        EventTime = str(Times[j])
+                        Event = str(Events[j])
+                        CurrentI = str(Is[j])
+                        Threshold = str(Thresh)
+                        f.write(n + "," + m + "," + p + "," + Threshold + "," +
+                                Beta + "," + SimNum + "," + EventTime + "," +
+                                Event + "," + CurrentI + "\n")
+
+
+f.close()
+
+logging.info("All Done! :-D")
---
+++ SimulationCode/SimpleGrid.py
@@ -0,0 +1,59 @@
+import CreateNetworks as CN
+import Simulation as S
+import logging
+import numpy as np
+import Path
+
+Ns = [40,50,100,250,500]
+Ms = [25,20,10,4,2]
+Periodic = [True,False]
+
+betas = [1.5]
+gamma = 1
+
+NumSims = 50
+InitialFrac = 0.01
+StoppingTime = 5
+
+path = Path.GetPath()
+
+logging.basicConfig(filename = path + "Logs/SimpleSimSquareGrid.log",
+                    format = '%(asctime)s - %(message)s',
+                    level = logging.INFO)
+
+f = open(path + "SimulationResults/SimpleContagionSimulations_SquareGrid.csv","w+")
+
+f.write("N,M,Periodic,beta,SimNum,EventTime,Event,CurrentI\n")
+
+TotalSims = len(Ns) * len(Periodic) * len(betas) * NumSims
+CurrentSim = float(1)
+
+logging.info("About to run simple sims for Square Grid.")
+
+for P in Periodic:
+    for k in range(len(Ns)):
+        for beta in betas:
+            for i in range(1,NumSims+1):
+                logging.info("Currently at " + str(100*CurrentSim/float(TotalSims)) + "% of the Simulations Done.\n")
+                CurrentSim = CurrentSim + 1
+
+                G = CN.GridGraph(Ns[k],Ms[k],P)
+
+                Times,Events,Is = S.SimpleSim(G,InitialFrac,StoppingTime,gamma,beta)
+
+                for j in range(len(Times)):
+                    n = str(Ns[k])
+                    m = str(Ms[k])
+                    p = str(P)
+                    Beta = str(beta)
+                    SimNum = str(i)
+                    EventTime = str(Times[j])
+                    Event = str(Events[j])
+                    CurrentI = str(Is[j])
+                    f.write(n + "," + m + "," + p + "," + Beta + "," + SimNum +
+                    "," + EventTime + "," + Event + "," + CurrentI + "\n")
+
+
+f.close()
+
+logging.info("All Done! :-D")
